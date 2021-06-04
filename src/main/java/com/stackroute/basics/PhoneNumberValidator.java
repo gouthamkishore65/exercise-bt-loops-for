f8162bca6c1f8f557c518bdfbbce3abaa55a7ed0
gouthamkishore65 <68435446+gouthamkishore65@users.noreply.github.com>
2021-06-05 04:14:51 +0530

    Add files via upload

---
+++ src/main/java/com/stackroute/basics/PhoneNumberValidator.java
@@ -0,0 +1,27 @@
+package com.stackroute.basics;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class PhoneNumberValidator {
+    //Create Scanner object as instance variable
+    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        PhoneNumberValidator object = new PhoneNumberValidator();
+        String input = object.getInput();
+        boolean result = object.validatePhoneNumber(input);
+        object.displayResult(result);
+    }
+
+    public String getInput() {
+      return null;
+    }
+
+    public void displayResult(boolean result) {
+       
+    }
+
+    public boolean validatePhoneNumber(String s) {
+       return false;
+    }
+}
